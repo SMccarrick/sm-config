9716e556dee9f27f2f74c99f602db87b1da158c1
SMccarrick <smccarrick2804@gmail.com>
2020-08-05 18:54:55 +0100

    ✨- added exports for each config

---
+++ index.js
@@ -0,0 +1,9 @@
+const eslintrc = require("./.eslintrc.js");
+const prettier = require("./.prettier-config.js");
+const tsconfig = require("./.tsconfig.json");
+
+module.exports = {
+  eslintrc,
+  prettier,
+  tsconfig,
+};
